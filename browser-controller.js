749ebba680fb154d23cafad2f77b9483db8dab4c
조병철 <jobc90@gmail.com>
2024-05-27 10:39:22 +0900

    엔터코드 추가

--- browser-controller.js
+++ browser-controller.js
@@ -26,9 +26,10 @@ const createBrowsers = async () => {
       let hashCode = crypto.randomBytes(20).toString("hex");
       const browser = await puppeteer.launch({
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        headless: process.env.HEADLESS === "true" ? true : false,
+        headless: true,
         protocolTimeout: 30000,
         executablePath: "/opt/homebrew/bin/chromium",
+        // executablePath: "/snap/bin/chromium",
       });
       browserMap.set(hashCode, browser);
     });
@@ -130,6 +131,9 @@ const businessPreparation = async (hashCode) => {
       );
 
       await homtaxPage.waitForTimeout(1000);
+      await homtaxPage.keyboard.press("Enter");
+      await homtaxPage.waitForTimeout(1000);
+      await homtaxPage.keyboard.press("Enter");
       const veraport = await homtaxPage.waitForSelector("#textbox1");
       await veraport.click();
       await homtaxPage.waitForTimeout(15000);
@@ -137,6 +141,7 @@ const businessPreparation = async (hashCode) => {
       const frame = await homtaxPage
         .frames()
         .find((frame) => frame.name() === "txppIframe");
+
       const safeText = await frame.$eval(
         "#object_status_E-SAFER-NX",
         (element) => element.textContent
@@ -152,6 +157,9 @@ const businessPreparation = async (hashCode) => {
       console.log("Element text:", ipinsideText);
       const ipinside = await frame.waitForSelector("#download_IPINSIDE-NX");
       await ipinside.click();
+      //   await frame.evaluate(() => {
+      //     document.querySelector('#download_IPINSIDE-NX').click();
+      // });
       await homtaxPage.waitForTimeout(1000);
       const magicText = await frame.$eval(
         "#object_status_MAGIC-XML-NX",
